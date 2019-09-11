68b5164855a8cc04005c9651ccf2bc51fc8562f0
Devon Marantz <devonmarantz@gmail.com>
2019-09-10 17:02:59 -0700

    Add board without styling and play game function

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-board.component.scss']
 })
 export class GameBoardComponent implements OnInit {
+  PlayerOne = { name: "Player 1", symbol: 'X'};
+  PlayerTwo = { name: "Player 2", symbol: 'O'};
+
+  board: string[];
+  winner: string;
+  playing: boolean;
+  currentPlayer: object;
 
   constructor() { }
 
   ngOnInit() {
+    this.playGame();
+  }
+
+  // Intializes game
+  playGame() {
+
+    this.board = [
+      '', '', '',
+      '', '', '',
+      '', '', '',
+    ];
+    this.winner = null;
+    this.playing = true;
+
+    this.currentPlayer = this.PlayerOne;
+
   }
 
 }
