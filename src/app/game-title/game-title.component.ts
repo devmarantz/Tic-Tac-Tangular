d9da316349e9f3fd110b7c77a00806f78eac5f43
Devon Marantz <devonmarantz@gmail.com>
2019-09-19 16:03:56 -0700

    Add ternary when it is past the first game

--- src/app/game-title/game-title.component.ts
+++ src/app/game-title/game-title.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GameTitleComponent implements OnInit {
   @Input() darkMode;
+  @Input() showScore: boolean;
+  @Input() score: object;
+
 
   constructor() { }
 
